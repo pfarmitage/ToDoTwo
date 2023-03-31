dcd5b1e8ce4547da5d691c9b7c13875ba3d3f4fa
pfarmitage <47189004+pfarmitage@users.noreply.github.com>
2023-03-31 15:19:56 +0100

    1 file modified

--- components/ProgressBar/ProgressBar.tsx
+++ components/ProgressBar/ProgressBar.tsx
@@ -2,39 +2,49 @@ import React from 'react';
 import { Box, Typography } from '@mui/material';
 import { styled } from '@mui/system';
 
-const GradientProgressBar = styled(Box)`
+const ProgressBarSection = styled(Box)`
   height: 20px;
-  border-radius: 5px;
-  background-image: linear-gradient(
-    90deg,
-    green,
-    green ${(props) => props.completed}%,
-    blue ${(props) => props.completed}%,
-    blue ${(props) => props.completed + props.remaining}%,
-    grey ${(props) => props.completed + props.remaining}%,
-    grey ${(props) => props.completed + props.remaining + props.underVelocity}%,
-    red ${(props) => props.completed + props.remaining + props.underVelocity}%,
-    red ${(props) => props.completed + props.remaining + props.underVelocity + props.overVelocity}%
-  );
+  position: absolute;
+  top: 0;
+  border-radius: 0px;
 `;
 
 const ProgressBar = ({ totalPoints, velocity, completedPoints }) => {
   const barLength = Math.max(velocity, totalPoints);
-  const completed = (completedPoints/barLength)*100;
-  const remaining = (Math.min(totalPoints-completedPoints,velocity-completedPoints))/barLength*100;
-  const underVelocity = (Math.max(velocity - totalPoints, 0)/barLength)*100;
-  const overVelocity = (Math.max(totalPoints - velocity, 0)/barLength)*100;
+  const completed = (completedPoints / barLength) * 100;
+  const remaining = (Math.max(Math.min(totalPoints - completedPoints, velocity - completedPoints), 0) / barLength) * 100;
+  const underVelocity = (Math.max(velocity - totalPoints, 0) / barLength) * 100;
+  const overVelocity = (Math.max(Math.min(totalPoints - velocity, totalPoints - completedPoints), 0) / barLength) * 100;
 console.log(barLength,completed,remaining,underVelocity,overVelocity)
   return (
     <Box>
       <Typography gutterBottom>Today's Progress</Typography>
-      <GradientProgressBar
-        completed={completed}
-        remaining={remaining}
-        underVelocity={underVelocity}
-        overVelocity={overVelocity}
-      />
-      <Box display="flex" justifyContent="space-between">
+      <Box position="relative" width="100%">
+        <ProgressBarSection
+          width={`${completed}%`}
+          bgcolor="green"
+          title={`Completed: ${completedPoints}`}
+        />
+        <ProgressBarSection
+          width={`${remaining}%`}
+          bgcolor="blue"
+          title={`Remaining: ${totalPoints - completedPoints}`}
+          style={{ left: `${completed}%` }}
+        />
+        <ProgressBarSection
+          width={`${underVelocity}%`}
+          bgcolor="grey"
+          title={`Spare Capacity: ${velocity - totalPoints}`}
+          style={{ left: `${completed + remaining}%` }}
+        />
+        <ProgressBarSection
+          width={`${overVelocity}%`}
+          bgcolor="red"
+          title={`Remaining: ${totalPoints - completedPoints}`}
+          style={{ left: `${completed + remaining + underVelocity}%` }}
+        />
+      </Box>
+      <Box display="flex" justifyContent="space-between" >
         <Typography>0</Typography>
         <Typography>{barLength}/{velocity}</Typography>
       </Box>
@@ -43,12 +53,3 @@ console.log(barLength,completed,remaining,underVelocity,overVelocity)
 };
 
 export default ProgressBar;
-
-
-/*
-const barLength = '1000'//Math.max(velocity, totalPoints);
-  const completedPercentage = '50' //(completedPoints / barLength) * 100;
-  const remainingPercentage = '80'//((totalPoints - completedPoints) / barLength) * 100;
-  const underVelocityPercentage = '10'//(Math.max(velocity - totalPoints, 0) / barLength) * 100;
-  const overVelocityPercentage = '10'//(Math.max(totalPoints - velocity, 0) / barLength) * 100;
-  */
